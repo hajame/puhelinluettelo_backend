56a2af5167c7a511a5984de19e19bc4df3e44594
Harri Mehtälä <harri.mehtala@helsinki.fi>
2018-12-09 15:41:52 +0200

    3.11 frontend build

---
+++ build/precache-manifest.6db8e71f4db426438f18585b16ba9959.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b99fa3fdf156dea04e77",
+    "url": "/static/js/main.b99fa3fd.chunk.js"
+  },
+  {
+    "revision": "6c8d705d7f2f1418263d",
+    "url": "/static/js/1.6c8d705d.chunk.js"
+  },
+  {
+    "revision": "b99fa3fdf156dea04e77",
+    "url": "/static/css/main.ad0f3895.chunk.css"
+  },
+  {
+    "revision": "065d6307199f1f0e8fb2e0fe160f4aca",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.86d1ce3ca9c0f05a099fe17a1daa210e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8abc35aed6196564dada",
+    "url": "/static/js/main.8abc35ae.chunk.js"
+  },
+  {
+    "revision": "6c8d705d7f2f1418263d",
+    "url": "/static/js/1.6c8d705d.chunk.js"
+  },
+  {
+    "revision": "8abc35aed6196564dada",
+    "url": "/static/css/main.ad0f3895.chunk.css"
+  },
+  {
+    "revision": "ca870f58e5ce4da88638070640da10ef",
+    "url": "/index.html"
+  }
+];
